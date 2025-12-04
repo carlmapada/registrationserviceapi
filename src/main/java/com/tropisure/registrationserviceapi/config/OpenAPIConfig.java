6ba6d7628310434be4709179c16820175e3390a1
carlmapada <carl.mapada@gmail.com>
2025-12-03 21:49:18 -0600

    [cmm]- added openapi config and dependencies

---
+++ src/main/java/com/tropisure/registrationserviceapi/config/OpenAPIConfig.java
@@ -0,0 +1,33 @@
+package com.tropisure.registrationserviceapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                )
+                .info(new Info().title("Registration Service API").version("1.0")
+                        .description("Tropisure RESTful API that handles the registration of users, agents, or entities."));
+    }
+}
+
